a3ac956b1952d9297fc5b7c37aac2e0e68164c68
unknown <sundp.kshetri@gmail.com>
2022-07-11 23:17:49 +0545

    ..

--- routes/dog.js
+++ routes/dog.js
@@ -4,7 +4,7 @@ const { createDog, getDogs } = require("../controllers/dog");
 
 const router = new express.Router();
 
-router.route("/").post(createDog.get(getDogs));
+router.route("/").post(createDog).get(getDogs);
 
 // router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
