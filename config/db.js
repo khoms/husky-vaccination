72ca6a266470759fece3109518379dc438722c9d
unknown <sundp.kshetri@gmail.com>
2022-07-11 21:04:35 +0545

    changes for heroku

--- config/db.js
+++ config/db.js
@@ -2,18 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://localhost:27017/Husky_Vaccination",
-      {
-        // const conn = await mongoose.connect(
-        //   "db link",
-        // {
-        useNewUrlParser: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false,
-        useUnifiedTopology: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.DB_LINK, {
+      // const conn = await mongoose.connect(
+      //   "db link",
+      // {
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
     console.log(`Database connected ${conn.connection.host}`);
   } catch (err) {
     console.log(err.message);
