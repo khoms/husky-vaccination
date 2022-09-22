44a9ceabdcbf3241309a43fb9272e3071f053841
unknown <sundp.kshetri@gmail.com>
2022-09-22 21:51:36 +0545

    All about sending mail functionality

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,8 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    // service: "gmail",
+    // secure: true,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
